c2ada8a46c47de1afe6a227e5c01aa0fe2b00bce
aaryan <aaryankalia165@gmail.com>
2026-04-28 18:41:20 +0530

    feat: Registered PlagiarismReport in admin

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import StudentRisk
+from .models import StudentRisk, PlagiarismReport
 
 
 @admin.register(StudentRisk)
@@ -7,3 +7,10 @@ class StudentRiskAdmin(admin.ModelAdmin):
     list_display = ['student', 'risk_level', 'risk_score', 'attendance_pct', 'avg_marks', 'failing_subjects', 'predicted_at']
     list_filter = ['risk_level', 'predicted_at']
     search_fields = ['student__user__username', 'student__roll_number']
+
+
+@admin.register(PlagiarismReport)
+class PlagiarismReportAdmin(admin.ModelAdmin):
+    list_display = ['assignment', 'submission_a', 'submission_b', 'similarity_score', 'checked_at']
+    list_filter = ['assignment', 'checked_at']
+    search_fields = ['assignment__title']
